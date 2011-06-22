44e878753ae85d2f56694666dcacee8abde39a57
Jeong YunWon <github@youknowone.org>
2011-06-22 17:37:31 +0900

    skeleton project page

--- charles.py
+++ charles.py
@@ -3,7 +3,10 @@ from flask import Flask
 app = Flask(__name__)
 
 from functools import wraps
+import flask
 from flask import render_template
+from flask import redirect
+from flask import url_for
 from flask import request, Response
 
 import database
@@ -23,6 +26,11 @@ def requires_auth(f):
         if not req_auth or\
             not auth.http_auth(req_auth.username, req_auth.password):
             return authenticate()
+        try:
+            database.User.by(name=req_auth.username)
+        except:
+            user = database.User(name=req_auth.username)
+            database.session.add(user)
         return f(*args, **kwargs)
     return decorated
 
@@ -35,6 +43,72 @@ def before_request():
 def index():
     
     return render_template('base.html')
+
+@app.route('/project')
+@app.route('/project/+')
+@app.route('/project/<name>')
+@requires_auth
+def project(name=''):
+    if not name: name = '+'
+    try:
+        proj = database.Project.by(name=name)
+    except:
+        flask.abort(404)
+    return render_template('project.html', project=proj)
+
+@app.route('/project/<name>/add-project', methods=['POST'])
+@requires_auth
+def project_add_subproj(name):
+    proj = database.Project.by(name=name)
+    childname = request.form['name']
+    child = database.Project(childname, proj.id)
+    database.session.add(child)
+    return redirect(url_for('project', name=childname))
+
+@app.route('/project/<name>/add-member', methods=['POST'])
+@requires_auth
+def project_add_member(name):
+    proj = database.Project.by(name=name)
+    try:
+        user = database.User.by(name=request.form['user'])
+    except:
+        flask.abort(400)
+    member = database.Member.filter_by(project_id=proj.id, user_id=user.id)
+    if member.count() > 0:
+        flask.abort(400)
+    membername = request.form['name']
+    if not membername:
+        membername = None
+    member = database.Member(proj.id, user.id, membername)
+    database.session.add(member)
+    return redirect(url_for('project', name=name))
+
+@app.route('/project/<name>/add-job', methods=['POST'])
+@requires_auth
+def project_add_job(name):
+    proj = database.Project.by(name=name)
+    jobname = request.form['name']
+    if not jobname:
+        flask.abort(400)
+    job = database.Job.filter_by(project_id=proj.id, name=jobname)
+    if job.count() > 0:
+        flask.abort(400)
+    job = database.Job(proj.id, jobname)
+    database.session.add(job)
+    return redirect(url_for('project', name=name))
+
+@app.route('/user/<name>')
+@requires_auth
+def user(name):
+    return render_template("base.html")
+
+def user_select_form(name):
+    form = '<select name="%s">' % name
+    for user in database.User.all():
+        form += '<option value="%s">%s</option>' % (user.name, user.name)
+    form += '</select>'
+    return flask.Markup(form)
+app.jinja_env.globals.update(user_select_form=user_select_form)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=7111, debug=True)
--- database.py
+++ database.py
@@ -33,12 +33,27 @@ class Project(Base):
     project_id = Column(Integer, primary_key=True)
     id = synonym('project_id')
     parent_id = Column(Integer) # null이면 최상위를, 아니면 project_id 참조
-    name = Column(Unicode(256))
+    name = Column(Unicode(256), nullable=False)
     
     jobs = relationship('Job', backref='project',
-                        primaryjoin='Job.project_id == Project.id')
+        primaryjoin='Job.project_id == Project.project_id')
     members = relationship('Member', backref='project',
-                           primaryjoin='Member.project_id == Project.id')
+        primaryjoin='Member.project_id == Project.project_id')
+
+    @property
+    def parent(self):
+        return Project.by(project_id=self.parent_id)
+
+    @property
+    def children(self):
+        return Project.filter(Project.parent_id == self.project_id)
+
+    def __init__(self, name, parent_id=None):
+        self.name = name
+        self.parent_id = parent_id
+
+    def __repr__(self):
+        return "<Project(%d<%d,'%s')>" % (self.id, self.parent_id, self.name)
 
 class Job(Base):
     """Job은 Project를 찰지게 만들기 위해 찰싹거릴 수 있는 대상이다."""
@@ -46,11 +61,18 @@ class Job(Base):
 
     job_id = Column(Integer, primary_key=True)
     id = synonym('job_id')
-    project_id = Column(Integer, ForeignKey(Project.id))
-    name = Column(Unicode(256))
+    project_id = Column(Integer, ForeignKey(Project.project_id), nullable=False)
+    name = Column(Unicode(256), nullable=False)
 
     votes = relationship('Vote', backref='job',
-                         primaryjoin='Vote.job_id == Job.id')
+                         primaryjoin='Vote.job_id == Job.job_id')
+
+    def __init__(self, project_id, name):
+        self.project_id = project_id
+        self.name = name
+
+    def __repr__(self):
+        return "<Job(%d,'%s')>" % (self.project_id, self.name)
 
 class User(Base):
     """User는 어떤 Project의 Member로 참여하여 Project를 찰지게 하거나
@@ -63,23 +85,43 @@ class User(Base):
     
     user_id = Column(Integer, primary_key=True)
     id = synonym('user_id')
-    name = Column(Unicode(64)) # langdev username
+    name = Column(Unicode(64), nullable=False) # langdev username
+    
+    members = relationship('Member', backref='user',
+        primaryjoin='Member.user_id == User.user_id')
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return "<User(%d,'%s')>" % (self.user_id, self.name)
 
 class Member(Base):
     """Member는 어떤 Project를 찰지게 만드는 또는 만들 User이다."""
     __tablename__ = 'project_members'
    
-    project_id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, primary_key=True)
+    project_id = Column(Integer, ForeignKey(Project.project_id),
+                        primary_key=True)
+    user_id = Column(Integer, ForeignKey(User.user_id),
+                     primary_key=True)
     name = Column(Unicode(128))
+    
+
+    def __init__(self, project_id, user_id, name=None):
+        self.project_id = project_id
+        self.user_id = user_id
+        self.name = name
+
+    def __repr__(self):
+        return "<Member(%d,%d,'%s')>" % (self.project_id, self.user_id, name)
 
 class Vote(Base):
     """Vote는 User가 어떤 Project를 찰지게 하기 위해 찰싹거리는 단위이다."""
     __tablename__ = 'votes'
 
-    job_id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, primary_key=True)
-    comment = Column(UnicodeText)
+    job_id = Column(Integer, ForeignKey(Job.job_id), primary_key=True)
+    user_id = Column(Integer, ForeignKey(User.user_id), primary_key=True)
+    comment = Column(UnicodeText, nullable=False)
 
 
 """Add shortcut methods filter_by, filter, by to each declarative class.
